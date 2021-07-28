d4119339818d9758174a50e40cae0eb45503275d
sukrubeyy <sukru.beyy@outlook.com>
2021-07-29 01:22:40 +0300

    The first chunk created

--- Minecraft_File/Assets/Scripts/Chunk.cs
+++ Minecraft_File/Assets/Scripts/Chunk.cs
@@ -7,25 +7,75 @@ public class Chunk : MonoBehaviour
     public MeshFilter meshFilter;
     public MeshRenderer meshRenderer;
 
-    
+    int verticesIndex = 0;
+    List<Vector3> voxelVertices = new List<Vector3>();
+    List<int> voxelTriangels = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    bool[,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
     void Start()
     {
-        int verticesIndex = 0;
-        List<Vector3> voxelVertices = new List<Vector3>();
-        List<int> voxelTriangels = new List<int>();
-        List<Vector2> uvs = new List<Vector2>();
+        PopulateVoxelMap();
+        CreateChunkMesh();  
+        CreateMesh();
+    }
 
+    public void CreateDataMesh(Vector3 pos)
+    {
         for (int p = 0; p < 6; p++)
         {
-            for (int i = 0; i < 6; i++)
+            //Eğer Çizdirilen Voxel'in arka yüzünde Bir Voxel Varsa Bunu Çizdirmesin.
+            //It's gonna check the voxel behind it if it returns true that means as a voxel there because of this
+            //we don't need to draw draw this face so skip over it if it returns false it means there's nothing there 
+            if (!CheckVoxel(pos + VoxelData.faceChek[p]))
             {
-                int triangelsIndex = VoxelData.voxelTris[p, i];
-                voxelVertices.Add(VoxelData.voxelVertices[triangelsIndex]);
+                voxelVertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTris[p, 0]]);
+                voxelVertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTris[p, 1]]);
+                voxelVertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTris[p, 2]]);
+                voxelVertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTris[p, 3]]);
+                uvs.Add(VoxelData.uvVertices[0]);
+                uvs.Add(VoxelData.uvVertices[1]);
+                uvs.Add(VoxelData.uvVertices[2]);
+                uvs.Add(VoxelData.uvVertices[3]);
                 voxelTriangels.Add(verticesIndex);
-                uvs.Add(VoxelData.uvVertices[i]);
-                verticesIndex++;
+                voxelTriangels.Add(verticesIndex+1);
+                voxelTriangels.Add(verticesIndex+2);
+                voxelTriangels.Add(verticesIndex+2);
+                voxelTriangels.Add(verticesIndex+1);
+                voxelTriangels.Add(verticesIndex+3);
+                verticesIndex += 4;
+            }
+           
+        }
+    }
+    public void PopulateVoxelMap()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    voxelMap[x, y, z] = true;
+                }
             }
         }
+    }
+    public void CreateChunkMesh()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    CreateDataMesh(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
+    public void CreateMesh()
+    {
 
         Mesh mesh = new Mesh();
         mesh.vertices = voxelVertices.ToArray();
@@ -35,5 +85,15 @@ public class Chunk : MonoBehaviour
         meshFilter.mesh = mesh;
     }
 
+    bool CheckVoxel(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
 
+        if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)
+         return false;
+
+        return voxelMap[x,y,z];
+    }
 }
--- Minecraft_File/Assets/Scripts/VoxelData.cs
+++ Minecraft_File/Assets/Scripts/VoxelData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public static class VoxelData
 {
+    public static readonly int ChunkWidth = 5;
+    public static readonly int ChunkHeight = 15;
+
     public static readonly Vector3[] voxelVertices = new Vector3[8] {
             new Vector3(0.0f,0.0f,0.0f),
             new Vector3(1.0f,0.0f,0.0f),
@@ -15,24 +18,30 @@ public static class VoxelData
             new Vector3(0.0f,1.0f,1.0f),
     };
 
-    public static readonly int[,] voxelTris = new int[6, 6] {
-
-              {0,3,1,1,3,2},    //Back Face
-              {5,6,4,4,6,7},    //Front face
-              {3,7,2,2,7,6},    //Top Face -- 
-              {1,5,0,0,5,4},    //Bottom Face
-              {4,7,0,0,7,3},    //Left Face
-              {1,2,5,5,2,6}     //Right Face
+    public static readonly Vector3[] faceChek = new Vector3[6] {
+    new Vector3(0,0,-1),//Back Face
+    new Vector3(0,0,1),//Front Face
+    new Vector3(0,1,0),//Top Face
+    new Vector3(0,-1,0),//Bottom Face
+    new Vector3(-1,0,0),//Left Face
+    new Vector3(1,0,0)//Right Face
     };
 
-    public static readonly Vector2[] uvVertices = new Vector2[6]
+    public static readonly int[,] voxelTris = new int[6, 4] {
+
+              {0,3,1,2},    //Back Face
+              {5,6,4,7},    //Front face
+              {3,7,2,6},    //Top Face -- 
+              {1,5,0,4},    //Bottom Face
+              {4,7,0,3},    //Left Face
+              {1,2,5,6}     //Right Face
+    };
+
+    public static readonly Vector2[] uvVertices = new Vector2[4]
         {
         new Vector2(0f,0f),
         new Vector2(0f,1f),
         new Vector2(1f,0f),
-
-        new Vector2(1f,0f),
-        new Vector2(0f,1f),
         new Vector2(1f,1f)
         };
 }
